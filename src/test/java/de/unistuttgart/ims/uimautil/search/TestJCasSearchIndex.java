665c4f9774d79317ca523b0d8b1406c4131a53d4
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-07-09 18:41:44 +0200

    fixed settings bug

--- src/test/java/de/unistuttgart/ims/uimautil/search/TestJCasSearchIndex.java
+++ src/test/java/de/unistuttgart/ims/uimautil/search/TestJCasSearchIndex.java
@@ -38,7 +38,7 @@ public class TestJCasSearchIndex {
 				AnalysisEngineFactory.createEngineDescription(MateLemmatizer.class));
 		jcas = iterable.iterator().next();
 		index = new JCasSearchIndex<Token>(Token.class);
-		index.addIndexFeatureName("lemma/value");
+		index.addIndexFeatureName("/lemma/value");
 	}
 
 	@SuppressWarnings("unchecked")
